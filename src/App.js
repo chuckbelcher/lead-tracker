ce92174f5cf29da5acb163e5be3eee6fde298c23
Chuck Belcher <cfbelcher@pateogroup.com>
2021-09-21 00:02:07 -0400

    added leads as a requiremewnt to useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react/cjs/react.development';
+import { useState, useEffect } from 'react/cjs/react.development';
 //import LeadList from './components/LeadList';
 import './App.css';
 
