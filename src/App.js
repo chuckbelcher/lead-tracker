ce57601e923568eb5301ff3cd2b907a7b8b1ab14
Chuck Belcher <cfbelcher@pateogroup.com>
2021-09-20 23:43:33 -0400

    commented out useEffect load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react/cjs/react.development';
+//import { useEffect } from 'react/cjs/react.development';
 //import LeadList from './components/LeadList';
 import './App.css';
 
