4f390ef4f8e5fea72d85ff0147d2427681f77593
Chuck Belcher <cfbelcher@pateogroup.com>
2021-09-21 00:00:12 -0400

    added leads as a requiremewnt to useEffect

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
 
   const loadLeads = async () => {
     try {
-      //await fetch('/.netlify/functions/getLeads');
+      await fetch('/.netlify/functions/getLeads');
       const res = await fetch('/.netlify/functions/getLeads');
       const leads = await res.json();
-      setLeads(leads);
+      setLeads('chuck');
       console.log(leads);
       console.log("trying load leads")
     }  catch (err) {
@@ -19,11 +19,10 @@ function App() {
     } 
   }
   
-  loadLeads();
 
-  //useEffect(() => {
-  //  loadLeads();
-  //}, []); 
+  useEffect(() => {
+    loadLeads();
+  }, [leads]); 
 
   console.log(leads);
   return (
