a530fdd9b16f7452c60681e4491558e3f3b551c5
Chuck Belcher <cfbelcher@pateogroup.com>
2021-09-23 12:56:36 -0400

    commented out entire loadLeads function and replaced it with a simple console.log statement

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import { useState, useEffect } from 'react/cjs/react.development';
-import LeadList from './components/LeadList';
+//import LeadList from './components/LeadList';
 import './App.css';
 
 function App() {
   const [ leads, setLeads ] = useState([]);
 
-  const loadLeads = async () => {
+  /* const loadLeads = async () => {
     try {
       const res = await fetch('/.netlify/functions/getLeads');
+      console.log(typeof res);
       const leads = await res.json();
+      console.log("running typ of")
       setLeads(leads);
       console.log("load leads")
     }  catch (err) {
       console.log('ERROR in loadLeads')
       console.error(err);
     } 
+  } */
+
+  const loadLeads = () => {
+    setLeads({
+      name: "chuck",
+      description: "something"
+    });
+
+    console.log(leads)
   }
   
 
   useEffect(() => {
     loadLeads();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []); 
 
-  console.log(leads);
+  //console.log(leads);
   return (
     <div className="container py-5">
       <h1 className="text-center mb-5">Sudzy Pets Lead Tracking System</h1>
       <div className="container">
-        <LeadList leads={leads} refreshLeads={loadLeads} />
+        {/* <LeadList leads={leads} refreshLeads={loadLeads} /> */}
       </div>
     </div>
   );
