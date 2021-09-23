81dae5b441fa078d5b0ac646fdc6554cf792df29
Chuck Belcher <cfbelcher@pateogroup.com>
2021-09-23 14:16:21 -0400

    pared down App.js to bare min

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import { useEffect } from 'react/cjs/react.development';
-//import LeadList from './components/LeadList';
-import './App.css';
 
 function App() {
-  //const [ leads, setLeads ] = useState([]);
 
-  /* const loadLeads = async () => {
-    try {
-      const res = await fetch('/.netlify/functions/getLeads');
-      console.log(typeof res);
-      const leads = await res.json();
-      console.log("running typ of")
-      setLeads(leads);
-      console.log("load leads")
-    }  catch (err) {
-      console.log('ERROR in loadLeads')
-      console.error(err);
-    } 
-  } */
-
-  
-  
   useEffect(() => {
     const loadLeads = () => {
       console.log("bla bla bla")
@@ -28,13 +9,9 @@ function App() {
     loadLeads();
   }, []); 
 
-  //console.log(leads);
   return (
     <div className="container py-5">
-      <h1 className="text-center mb-5">Sudzy Pets Lead Tracking System</h1>
-      <div className="container">
-        {/* <LeadList leads={leads} refreshLeads={loadLeads} /> */}
-      </div>
+      <h1 className="text-center mb-5">Hello World</h1>
     </div>
   );
 }
