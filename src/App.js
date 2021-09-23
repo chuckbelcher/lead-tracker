f3b02afcf2b7c69ea75f0d8f65a5747f5f15dc1a
Chuck Belcher <cfbelcher@pateogroup.com>
2021-09-23 13:24:42 -0400

    moved function to useEffect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import { useEffect } from 'react/cjs/react.development';
+//import LeadList from './components/LeadList';
 import './App.css';
 
 function App() {
+  //const [ leads, setLeads ] = useState([]);
 
-  const loadLeads = () => {
-    console.log("bla bla bla")
-  }
+  /* const loadLeads = async () => {
+    try {
+      const res = await fetch('/.netlify/functions/getLeads');
+      console.log(typeof res);
+      const leads = await res.json();
+      console.log("running typ of")
+      setLeads(leads);
+      console.log("load leads")
+    }  catch (err) {
+      console.log('ERROR in loadLeads')
+      console.error(err);
+    } 
+  } */
+
+  
   
   useEffect(() => {
+    const loadLeads = () => {
+      console.log("bla bla bla")
+    }
     loadLeads();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []); 
 
+  //console.log(leads);
   return (
     <div className="container py-5">
       <h1 className="text-center mb-5">Sudzy Pets Lead Tracking System</h1>
+      <div className="container">
+        {/* <LeadList leads={leads} refreshLeads={loadLeads} /> */}
+      </div>
     </div>
   );
 }
