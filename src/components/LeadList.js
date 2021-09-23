11813e796311faa6519a6225907cf9a49318b4f2
Chuck Belcher <cfbelcher@pateogroup.com>
2021-09-23 16:07:52 -0400

    updated LeadList to show archived records

--- src/components/LeadList.js
+++ src/components/LeadList.js
@@ -9,7 +9,9 @@ export default function LeadList({leads, refreshLeads}) {
                     .map(lead => <LeadCard key={lead._id} lead={lead} refreshLeads={refreshLeads}/>)}
                 
                 <h3 className="mt-5">Archived Leads</h3>
-                {/* {links && links.filter(link => link.archived).map(link => <LinkCard key={link._id} link={link} refreshLinks={refreshLinks}/>)} */}
+                {leads && leads.filter(lead => lead.archived)
+                    .map(lead => <LeadCard key={lead._id} lead={lead} refreshLeads={refreshLeads}/>)}
+                
         </div>
     )
 }
